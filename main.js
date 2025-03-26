16979cf3b8cb6adb653729ecc217ee73a01cc006
samuel <samuel@teste.com>
2025-03-26 20:13:11 -0300

    try and catch

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const nameElement = document.querySelector('#name');
-    const usernameElement = document.querySelector('#username');
-    const avatarElement = document.querySelector('#avatar');
-    const reposElement = document.querySelector('#repos');
-    const followersElement = document.querySelector('#followers');
-    const followingElement = document.querySelector('#following');
-    const linkElement = document.querySelector('#link');
+document.addEventListener('DOMContentLoaded', function () {
+    try {
+        const nameElement = document.querySelector('#name');
+        const usernameElement = document.querySelector('#username');
+        const avatarElement = document.querySelector('#avatar');
+        const reposElement = document.querySelector('#repos');
+        const followersElement = document.querySelector('#followers');
+        const followingElement = document.querySelector('#following');
+        const linkElement = document.querySelector('#link');
 
-    fetch('https://api.github.com/users/samueltonon')
-        .then(function(res) {
-            return res.json();
-        })
-        .then(function(json) {
-            nameElement.innerText = json.name;
-            usernameElement.innerText = json.login;
-            avatarElement.src = json.avatar_url;
-            repos.innerText = json.public_repos;
-            followersElement.innerText = json.followers;
-            followingElement.innerText = json.following;
-            linkElement.href = json.html_url;
-        })
-})
+        fetch('https://api.github.com/users/samueltonon')
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (json) {
+                nameElement.innerText = json.name;
+                usernameElement.innerText = json.login;
+                avatarElement.src = json.avatar_url;
+                reposElement.innerText = json.public_repos;
+                followersElement.innerText = json.followers;
+                followingElement.innerText = json.following;
+                linkElement.href = json.html_url;
+            })
+            .catch(function(erro) {
+                alert('Erro ao buscar dados.')
+            })
+    } catch (erro) {
+        alert('Ocorreu um erro, tente novamente.');
+    }
+});
